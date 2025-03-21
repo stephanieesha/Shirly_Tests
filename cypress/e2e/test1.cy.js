2308ca72487765608dc98deef961b173a67f3f37
Stephanie Ayuba Esha <stephanieesha@gmail.com>
2025-03-21 06:18:55 +0100

    added test to start render instance

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,6 +1,10 @@
 import overviewPage from "../pageObject/overviewPage"
 
 describe('Test User Functionalities on Shirly', () => {
+    it('should load website and start up render instance', () =>{
+        cy.visit('https://shirly.onrender.com/')
+    })
+
     it('should register user, login user, create a new ListName, edit ListName, Create a new List, verify List details, delete List, delete ListName, logout user', () =>{
         cy.visit('https://shirly.onrender.com/')
         overviewPage.whenClickRegisterButton()
